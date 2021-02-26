947a9e8e4816e81a72f4b4f070b482e6ce3be2d0
Elias Bendjaballah <azwbdj@hotmail.fr>
2021-02-26 23:07:01 +0100

    WORK

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-{/*import { Button } from './Button';*/}
+import { Button } from './Button';
 import './Navbar.css';
 
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const [{/*button*/}, setButton] = useState(true);
+    const [button, setButton] = useState(true);
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
