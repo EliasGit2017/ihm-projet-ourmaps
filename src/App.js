e2f50d54f7eba3f7ca4817c68e13da9887609de3
Elias Bendjaballah <azwbdj@hotmail.fr>
2021-02-27 15:13:10 +0100

    add

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import SignUp from './Components/pages/SignUp'
 function App() {
   return (
     <>
-      <Router basename="/ihm-projet-ourmaps">
+      <Router >
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
